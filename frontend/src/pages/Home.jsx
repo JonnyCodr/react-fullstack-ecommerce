c7521afecd58c16b82a609f5ced00f5ef3735c60
michael.menard@dhigroupinc.com <michael.menard@dhigroupinc.com>
2022-11-08 18:12:23 -0700

    updates homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import ProductCarousel from "../components/layout/ProductCarousel";
+import CategoryCard from "../components/CategoryCard";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
 
 const Home = () => {
+  const productCategories = [
+    'Tablets',
+    'Monitors',
+    'Games',
+    'Printers',
+    'Software',
+    'Cameras',
+    'Books',
+    'Videos',
+  ];
+
   return (
-    <div>
-      <h1>Home</h1>
+    <>
       <ProductCarousel />
-    </div>
+      <Container>
+      <Row xs={1} md={2} className="g-4 mt-5">
+      {
+        productCategories.map((category, index) => (
+          <CategoryCard category={category} idx={index} key={index} />
+        ))
+      }
+      </Row>
+      </Container>
+    </>
   );
 };
 
