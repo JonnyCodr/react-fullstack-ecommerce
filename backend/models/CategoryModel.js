1ba5573f537d3b2b799ac44e2763f0f0f856a09a
michael.menard@dhigroupinc.com <michael.menard@dhigroupinc.com>
2022-11-19 19:47:46 -0700

    adds seeder for category

--- backend/models/CategoryModel.js
+++ backend/models/CategoryModel.js
@@ -1,22 +1,12 @@
 const mongoose = require("mongoose");
 
 const categorySchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  description: {
-    type: String,
-    default: "default category description",
-  },
-  image: {
-    type: String,
-    default: "/images/tablet-category.png",
-  },
+  name: { type: String, required: true, unique: true },
+  description: { type: String, default: "default category description" },
+  image: { type: String, default: "/images/tablets-category.png" },
   attrs: [
-    { key: { type: String }, value: { type: String } },
-    ]
+    { key: { type: String }, value: [{ type: String }]
+    }],
 });
 
 const Category = mongoose.model("Category", categorySchema);
