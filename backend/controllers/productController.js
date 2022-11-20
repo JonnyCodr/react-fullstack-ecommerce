3e1deddaea4ef5090e6732b8a4a29b0c1123bac3
michael.menard@dhigroupinc.com <michael.menard@dhigroupinc.com>
2022-11-20 12:45:03 -0700

    implements pagination and sorting of product results

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,29 @@
-const GetProducts = (req, res) => {
-  res.send("Sup. Handling product routes");
-};
+const Product = require("../models/ProductModel");
+const recordsPerPage = require("../config/pagination");
+
+
+const GetProducts = async (req, res, next) =>{
+  try {
+    const pageNum = parseInt(req.query.pageNum) || 1;
+    const totalProducts = await Product.countDocuments({});
+
+    // sort by name, price, etc
+    let sort = {};
+    const sortOption = req.query.sort || '';
+    if (sortOption) {
+      let sortOptionArray = sortOption.split('_');
+      sort = {[sortOptionArray[0]]: Number(sortOptionArray[1])};
+    }
+
+    const allProducts = await Product.find({})
+      .skip(recordsPerPage * (pageNum - 1))
+      .sort(sort)
+      .limit(recordsPerPage);
+
+    res.status(200).json({allProducts, pageNum, paginationLinksNumber: Math.ceil(totalProducts / recordsPerPage)});
+  } catch (error) {
+   next(error)
+  }
+}
 
 module.exports = GetProducts;
