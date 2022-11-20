94c6346eb2b98819ced4b8a19df3440e03a58333
michael.menard@dhigroupinc.com <michael.menard@dhigroupinc.com>
2022-11-20 12:22:30 -0700

    adds route to category for adding attributes to categories

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -39,8 +39,46 @@ const DeleteCategory = async (req, res, next) => {
   }
 }
 
+const SaveAttr = async (req, res, next) => {
+  const {key, val, categoryChosen} = req.body
+  if (!key || !val || !categoryChosen) {
+    return res.status(400).send("All inputs are required")
+  }
+
+  try {
+    const category = categoryChosen.split("/")[0]
+    const categoryExists = await Category.findOne({name: category}).orFail()
+    if(categoryExists.attrs.length > 0) {
+      // if key exists in the database then add a value to the key
+      var keyDoesNotExistsInDatabase = true
+      categoryExists.attrs.map((item, idx) => {
+        if(item.key === key) {
+          keyDoesNotExistsInDatabase = false
+          let copyAttributeValues = [...categoryExists.attrs[idx].value];
+          copyAttributeValues.push(val)
+          let newAttributeValues = [...new Set(copyAttributeValues)]; // Set ensures unique values
+          categoryExists.attrs[idx].value = newAttributeValues
+        }
+      })
+
+      if(keyDoesNotExistsInDatabase) {
+        categoryExists.attrs.push({key: key, value: [val]})
+      }
+    } else {
+      // push to the array
+      categoryExists.attrs.push({key: key, value: [val]})
+    }
+    await categoryExists.save()
+    let cat = await Category.find({}).sort({name: "asc"})
+    return res.status(201).json({categoriesUpdated: cat})
+  } catch(err) {
+    next(err)
+  }
+}
+
 module.exports = {
   GetCategories,
   NewCategory,
-  DeleteCategory
+  DeleteCategory,
+  SaveAttr
 };
--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const {NewCategory, GetCategories, DeleteCategory} = require("../controllers/categoryController");
+const {
+  NewCategory,
+  GetCategories,
+  DeleteCategory,
+  SaveAttr
+} = require("../controllers/categoryController");
 
 router.get("/", GetCategories);
 router.post("/", NewCategory);
 router.delete("/:category", DeleteCategory);
+router.post("/attr", SaveAttr);
 
 module.exports = router;
