856b0e27be326fbc1a9328ca7204aa1d8d10177b
michael.menard@dhigroupinc.com <michael.menard@dhigroupinc.com>
2022-11-20 17:22:16 -0700

    adds ability to find product by id

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,52 +1,136 @@
 const Product = require("../models/ProductModel");
 const recordsPerPage = require("../config/pagination");
 
-
-const GetProducts = async (req, res, next) =>{
+const GetProducts = async (req, res, next) => {
+  console.log("req.params: ", req.params);
+  console.log("req.query: ", req.query);
+  console.log("req.query attrs: ", req.query.attrs);
   try {
-    let queryCondition = false;
     let query = {};
-    let priceQueryCondition = {};
-    let ratingQueryCondition = {};
+    let queryCondition = false;
 
+    let priceQueryCondition = {};
     if (req.query.price) {
       queryCondition = true;
-      priceQueryCondition = { price: { $lte: req.query.price } };
+      priceQueryCondition = { price: { $lte: Number(req.query.price) } };
     }
+    let ratingQueryCondition = {};
     if (req.query.rating) {
       queryCondition = true;
-      ratingQueryCondition = { rating: { $in: req.query.rating.split(',') } };
+      ratingQueryCondition = { rating: { $in: req.query.rating.split(",") } };
+    }
+    let categoryQueryCondition = {};
+    const categoryName = req.params.categoryName || "";
+    if (categoryName) {
+      queryCondition = true;
+      let a = categoryName.replaceAll(",", "/");
+      var regEx = new RegExp("^" + a);
+      categoryQueryCondition = { category: regEx };
+    }
+    if (req.query.category) {
+      queryCondition = true;
+      let a = req.query.category.split(",").map((item) => {
+        if (item) return new RegExp("^" + item);
+      });
+      categoryQueryCondition = {
+        category: { $in: a },
+      };
+    }
+    let attrsQueryCondition = [];
+    if (req.query.attrs) {
+      // attrs=RAM-1TB-2TB-4TB,color-blue-red
+      // [ 'RAM-1TB-4TB', 'color-blue', '' ]
+      console.log('incoming attrs:', req.query.attrs);
+
+      attrsQueryCondition = req.query.attrs.split(",").reduce((acc, item) => {
+        if (item) {
+          console.log('item:', item);
+          let a = item.split("-");
+          let values = [...a];
+          console.log('values before shift:', values);
+          values.shift(); // removes first item
+          console.log("values after shift:", values);
+          let a1 = {
+            attrs: { $elemMatch: { key: a[0], value: { $in: values } } },
+          };
+          console.log('a1:', a1);
+          acc.push(a1);
+          // console.dir(acc, { depth: null })
+          console.log('inside acc:', acc);
+          return acc;
+        } else {
+          console.log('outside acc:', acc);
+          return acc;
+        }
+      }, []);
+
+      console.log("attrsQueryCondition", attrsQueryCondition);
+      //   console.dir(attrsQueryCondition, { depth: null });
+      queryCondition = true;
+    }
+
+    //pagination
+    const pageNum = Number(req.query.pageNum) || 1;
+
+    // sort by name, price etc.
+    let sort = {};
+    const sortOption = req.query.sort || "";
+    if (sortOption) {
+      let sortOpt = sortOption.split("_");
+      sort = { [sortOpt[0]]: Number(sortOpt[1]) };
+    }
+
+    //search
+    let searchQueryCondition = {};
+    const searchQuery = req.params.searchQuery || "";
+    let select = {};
+    if (searchQuery) {
+      queryCondition = true;
+      // searchQueryCondition = { $text: { $search: '"' + searchQuery + '"' } }; // exact match
+      searchQueryCondition = { $text: { $search: searchQuery } };
+      select = { score: { $meta: "textScore" } };
+      sort = { score: { $meta: "textScore" } };
     }
 
     if (queryCondition) {
       query = {
-        $and: [priceQueryCondition, ratingQueryCondition]
-      }
-    }
-
-    console.log(query);
-
-    // pagination
-    const pageNum = parseInt(req.query.pageNum) || 1;
-
-    // sort by name, price, etc
-    let sort = {};
-    const sortOption = req.query.sort || '';
-    if (sortOption) {
-      let sortOptionArray = sortOption.split('_');
-      sort = {[sortOptionArray[0]]: Number(sortOptionArray[1])};
+        $and: [
+          priceQueryCondition,
+          ratingQueryCondition,
+          categoryQueryCondition,
+          searchQueryCondition,
+          ...attrsQueryCondition
+        ],
+      };
     }
 
     const totalProducts = await Product.countDocuments(query);
-    const allProducts = await Product.find(query)
+    const products = await Product.find(query)
+      .select(select)
       .skip(recordsPerPage * (pageNum - 1))
       .sort(sort)
       .limit(recordsPerPage);
 
-    res.status(200).json({allProducts, pageNum, paginationLinksNumber: Math.ceil(totalProducts / recordsPerPage)});
+    res.json({
+      products,
+      pageNum,
+      paginationLinksNumber: Math.ceil(totalProducts / recordsPerPage),
+    });
   } catch (error) {
-   next(error)
+    next(error);
   }
+};
+
+const GetProductById = async (req, res, next) => {
+  try {
+    const product = await Product.findById(req.params.id).populate('reviews').orFail();
+    res.json(product);
+
+  } catch (error) {
+    next(error);
+  }
+
 }
 
-module.exports = GetProducts;
+module.exports = {GetProducts, GetProductById};
+
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const GetProducts = require("../controllers/productController");
+const {
+  GetProducts,
+  GetProductById
+} = require("../controllers/productController");
 
+router.get('/category/:categoryName/search/:searchQuery', GetProducts);
+router.get("/category/:categoryName", GetProducts);
+router.get("/search/:searchQuery", GetProducts);
 router.get("/", GetProducts);
+router.get("/:id", GetProductById);
 
 module.exports = router;
