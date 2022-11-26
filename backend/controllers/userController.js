71bc98c94c7d3912ccddfe1ef31b0be3097b1ee2
michael.menard@dhigroupinc.com <michael.menard@dhigroupinc.com>
2022-11-26 13:20:08 -0700

    implements api endpoint to list all users

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,13 @@
-const GetProducts = (req, res) => {
-  res.send("Sup. Handling user routes");
+const User = require("../models/userModel");
+
+const GetUsers = async (req, res, next) => {
+  try {
+    const users = await User.find({}).select("-password");
+    return res.json(users);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
 };
 
-module.exports = GetProducts;
+module.exports = GetUsers;
